02aec75c30cd26b3c0ce8331697cbe62ebfc8427
chilledsunset <chilledsunset@gmail.com>
2023-04-19 15:07:28 +0300

    moneytransferservice

---
+++ src/test/java/com/rakitov/moneytransferservice/MoneytransferserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rakitov.moneytransferservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneytransferserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
